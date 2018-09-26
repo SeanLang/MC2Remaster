4744a800daa92dd8c2eaaa894ea38f85353604af
DropBearKarma <brandoncoverley@gmail.com>
2018-09-26 13:58:48 +0800

    Setting As Up To Date
    After a small mselt down by the github desktop software this is the up to date version of the program

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+   public static GameObject draggedObject;
+   public  GameObject CanvasTopLayer;
+   Vector3 returnPosition;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        transform.SetParent(CanvasTopLayer.transform);
+        draggedObject = this.gameObject;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    void Start()
+    {
+        returnPosition = transform.localPosition;
+    }
+
+    void Update()
+    {
+
+    }
+}
